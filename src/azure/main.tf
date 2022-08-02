b8f2282b3994caf1fbeca22e0544fb8e90aefab5
bczemp <ben@parigan.com>
2022-08-02 12:08:05 -0400

    Fixed formatting and added AFS var

--- src/azure/main.tf
+++ src/azure/main.tf
@@ -13,15 +13,15 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = var.subscription_id 
-  client_id       = var.client_id 
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
   client_secret   = var.client_secret
-  tenant_id       = var.tenant_id 
+  tenant_id       = var.tenant_id
 }
 
 resource "azurerm_resource_group" "rg" {
   name     = "JenkinsRG"
-  location = var.location 
+  location = var.location
 }
 
 resource "azurerm_container_registry" "acr" {
@@ -29,13 +29,13 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Premium"
-  admin_enabled = true
+  admin_enabled       = true
 
   network_rule_set {
     default_action = "Deny"
-    
+
     ip_rule {
-      action = "Allow"
+      action   = "Allow"
       ip_range = var.ip_range
     }
   }
--- src/azure/variables.tf
+++ src/azure/variables.tf
@@ -1,34 +1,40 @@
 variable "client_id" {
   description = "Client id value for AZ authentication"
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
 
 variable "client_secret" {
   description = "Client Secret for AZ authentication"
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
 
 variable "tenant_id" {
   description = "Tenant id for AZ authentication"
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
 
 variable "subscription_id" {
   description = "Subscription id for AZ authentication"
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
 
 variable "location" {
   description = "Cloud regions for AZ resources"
-  type = string
+  type        = string
 }
 
 variable "ip_range" {
   description = "CIDR range of IP addresses allowed to access resources"
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
+}
+
+variable "AFS_ON" {
+  description = "Controls creation of the Azure file share used during initial setup"
+  type        = bool
+  default     = false
 }
