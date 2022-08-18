8552d75e71a3400e20e379aac2bb24da9378ca84
bczemp <ben@parigan.com>
2022-08-18 13:30:01 -0400

    Added outputs for attaching kubectl with azure-cli

--- src/azure/main.tf
+++ src/azure/main.tf
@@ -76,3 +76,11 @@ output "kube_config" {
   value     = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
 }
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "kubernetes_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
