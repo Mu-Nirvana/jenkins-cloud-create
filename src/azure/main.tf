d96e78a6f4d6b68299cefb86e5cec54d7dc0637f
Nishan Wijesinghe <nishan@parigan.com>
2022-08-28 10:11:29 -0400

    https://app.asana.com/0/1200577571023559/1202377222536087/f: Documentation for future enhancements.

--- src/azure/main.tf
+++ src/azure/main.tf
@@ -18,6 +18,7 @@ provider "azurerm" {
   features {}
 
   subscription_id = var.subscription_id
+  #Required when AZ TF provider gets called from IaC CI/CD
   #client_id       = var.client_id
   #client_secret   = var.client_secret
   #tenant_id       = var.tenant_id
